d8e723963c50d3a38522e4d9b80411d9510d3438
Jason Selwyn <jdselwyn@gmail.com>
2025-08-29 14:45:58 -0400

    update erd
    remove shipments from main db and update sample_sites_sheets to use lot_id

--- scripts/assemble_db.R
+++ scripts/assemble_db.R
@@ -12,13 +12,13 @@ install_and_load_packages(cran_packages = c("tidyverse",
 db_with_pk <- compile_db_inputs() %>%
     do.call(dm, .) %>%
   dm_add_pk(sampling_sites_sheets,
-            columns = c(site_sp_primary_key)) %>%
+            columns = c(lot_id)) %>%
   dm_add_pk(lots_sheets,
             columns = c(lot_id)) %>%
   dm_add_pk(individuals_sheets,
             columns = c(individual_id)) %>%
   dm_add_pk(table = species_sheets,
-            columns = species_code) %>%
+            columns = species_valid_name) %>%
   dm_add_pk(dna_extractions_sheets,
             columns = c(extraction_id)) %>%
   dm_add_pk(table = dna_extractions_gels,
@@ -34,23 +34,23 @@ db_with_pk <- compile_db_inputs() %>%
 
 #### Add Foreign Keys ####
 pire_db <- db_with_pk %>%
-  dm_add_fk(table = sampling_sites_sheets,
-            columns = species_id,
-            ref_table = species_sheets) %>%
+  # dm_add_fk(table = sampling_sites_sheets,
+  #           columns = species_id,
+  #           ref_table = species_sheets) %>%
   dm_add_fk(table = lots_sheets, 
-            columns = site_id, 
+            columns = lot_id, 
             ref_table = sampling_sites_sheets,
-            ref_columns = site_id) %>%
+            ref_columns = lot_id) %>%
   dm_add_fk(table = individuals_sheets, 
             columns = lot_id, 
             ref_table = lots_sheets) %>%
+    dm_add_fk(table = individuals_sheets, 
+              columns = lot_id, 
+              ref_table = sampling_sites_sheets) %>%
   dm_add_fk(table = individuals_sheets,
             columns = species_valid_name,
             ref_table = species_sheets,
             ref_columns = species_valid_name) %>%
-  dm_add_fk(table = individuals_sheets, 
-            columns = collection_site, 
-            ref_table = sampling_sites_sheets) %>%
   dm_add_fk(table = dna_extractions_sheets, 
             columns = individual_id, 
             ref_table = individuals_sheets) %>%
@@ -64,13 +64,13 @@ rm(db_with_pk)
 #species_id
 
 #### visualize db ####
-# erd_image <-
-#   full_db %>%
-#   dm_draw(rankdir = "TB", view_type = "keys_only")
-# 
-# erd_image
-# 
-# full_db %>%
+erd_image <-
+    pire_db %>%
+    dm_draw(rankdir = "TB", view_type = "keys_only")
+
+erd_image
+
+# pire_db %>%
 #   dm_draw(rankdir = "TB", view_type = "all")
 
 
